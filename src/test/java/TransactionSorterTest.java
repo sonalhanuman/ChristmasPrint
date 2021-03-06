0f6f4564a3bf90a21798466dc6daba988ef0f6e6
sonalhanuman <sonalhanuman@gmail.com>
2021-03-07 01:58:22 +0800

    Adding Transaction Sorter Functionalities

--- src/test/java/TransactionSorterTest.java
+++ src/test/java/TransactionSorterTest.java
@@ -36,4 +36,23 @@ public class TransactionSorterTest {
 
         assertEquals(expectedTransactions, transactionSorted);
     }
+
+    @Test
+    public void sortWith3TransactionsShouldReturnTransactionsSortedByNameThenCategory() {
+        List<Transaction> transactions = new ArrayList<>();
+        Transaction transaction1 = new Transaction("Sonal", "Gadget", "2021/03/03", 25.00);
+        Transaction transaction2 = new Transaction("David", "Food", "2021/03/03", 15.00);
+        Transaction transaction3 = new Transaction("Sonal", "Food", "2021/03/03", 15.00);
+        transactions.add(transaction1);
+        transactions.add(transaction2);
+        transactions.add(transaction3);
+        List<Transaction> expectedTransactions = new ArrayList<>();
+        expectedTransactions.add(transaction2);
+        expectedTransactions.add(transaction3);
+        expectedTransactions.add(transaction1);
+
+        var transactionSorted = TransactionSorter.sort(transactions);
+
+        assertEquals(expectedTransactions, transactionSorted);
+    }
 }
